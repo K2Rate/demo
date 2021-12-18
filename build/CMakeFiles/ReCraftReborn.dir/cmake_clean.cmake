1383c6aa93deac392e949eb6ec24916f981c909c
K2Rate <dns.1983n@gmail.com>
2021-12-18 10:51:38 +0600

    plaki

---
+++ build/CMakeFiles/ReCraftReborn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReCraftReborn.dir/code/main.cpp.obj"
+  "ReCraftReborn.exe"
+  "ReCraftReborn.exe.manifest"
+  "ReCraftReborn.lib"
+  "ReCraftReborn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReCraftReborn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
